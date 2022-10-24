1c340f9e7ec0076ecb14c2c87a863bb1c09a4f72
dannysilence <48093199+dannysilence@users.noreply.github.com>
2022-10-24 08:21:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
- /// <reference types="cypress" />
+/// <reference types="cypress" />
 // @ts-check
 'use strict'
 
+const fs = require('fs')
 const path = require('path')
 const debug = require('debug')('cypress-failed-log')
 
@@ -10,32 +11,26 @@ if (typeof path.basename !== 'function') {
   throw new Error('path.basename should be a function')
 }
 
-const maxFileNameLength = 220
-const cleanupFilename = s => Cypress._.kebabCase(Cypress._.deburr(s))
-const truncateFilename = s => Cypress._.truncate(s, {
-  length: maxFileNameLength,
-  omission: ''
-})
-const getCleanFilename = s => truncateFilename(cleanupFilename(s))
 const getFilepath = filename => path.join('cypress', 'logs', filename)
-const retriesTimes = getRetriesTimes()
+// const retriesTimes = getRetriesTimes()
 
-function getRetriesTimes () {
-  const retries = Cypress.config('retries')
-  if (Cypress._.isNumber(retries)) {
-    return retries
-  }
+// function getRetriesTimes() {
+//   const retries = Cypress.config('retries')
+//   if (Cypress._.isNumber(retries)) {
+//     return retries
+//   }
 
-  if (Cypress._.isObject(retries) && Cypress._.isNumber(retries.runMode)) {
-    return retries.runMode
-  }
+//   if (Cypress._.isObject(retries) && Cypress._.isNumber(retries.runMode)) {
+//     return retries.runMode
+//   }
 
-  return 0
-}
+//   return 0
+// }
 
 const testCaseTable = {}
+const specData = { spec: '', tests: [] }
 
-function writeTestInfo ({
+function writeTestInfo({
   specName,
   title,
   suiteName,
@@ -44,24 +39,25 @@ function writeTestInfo ({
   testCommands
 }) {
   const info = {
-    specName,
-    title,
-    suiteName,
-    testName,
-    testError,
-    testCommands
+    // specName,
+    // title: title,
+    // suite: suiteName,
+    test: testName,
+    commands: Cypress._.map(testCommands, c => c.message)
   }
-  const str = JSON.stringify(info, null, 2) + '\n'
-  const cleaned = getCleanFilename(
-    Cypress._.join([
-      Cypress._.split(specName, '.')[0],
-      testName
-    ], '-'))
-  const filename = `${cleaned}.json`
+  if (testError) {
+    info.error = testError;
+  }
+
+  specData.spec = specName;
+  // @ts-ignore
+  specData.tests.push(info);
+
+  const str = JSON.stringify(specData, null, 2) + '\n'
+  const filename = specName.includes("\\") ? specName.replace("\\", "/") + `.json` : specName + '.json';
   const filepath = getFilepath(filename)
-  cy
-    .writeFile(filepath, str, {log:false})
-    //.log(`saved failed test information to ${filename}`)
+
+  cy.writeFile(filepath, str, { log: false })
 
   return filepath
 }
@@ -69,14 +65,20 @@ function writeTestInfo ({
 let savingCommands = false
 let loggedCommands = []
 
-function startLogging () {
+function startLogging() {
   debug('will log Cypress commands')
 
-  Cypress.on('test:before:run', () => {
+  Cypress.on('test:before:run', (attr, test) => {
     debug('before test run')
-   
+
     loggedCommands = []
     savingCommands = true
+  })
+
+  Cypress.on('test:after:run', (attr, test) => {
+    debug('after test run')
+
+    savingCommands = false
   })
 
   Cypress.on('log:added', options => {
@@ -106,11 +108,7 @@ function startLogging () {
   })
 }
 
-//function initLog () {
-//  loggedCommands = []
-//}
-
-function onFinish () {
+function onFinish() {
   savingCommands = false
 
   const testName = this.currentTest.fullTitle()
@@ -123,32 +121,29 @@ function onFinish () {
   }
 
   const title = this.currentTest.title
-
   const suiteName = this.currentTest.parent && this.currentTest.parent.title
-
-  const testError = (this.currentTest.state === 'passed') ? '' : this.currentTest.err.message
-
-  const commands = loggedCommands
-
-  const testCommands = commands;
-
+  const testError = (this.currentTest.state === 'passed' || this.currentTest.state === 'pending') ? '' : this.currentTest.err.message
+  const testCommands = loggedCommands
   const specName = Cypress.spec.relative
 
-  console.log('=== test failed ===')
-  console.log(specName)
-  console.log('=== title ===')
-  console.log(title)
-  if (suiteName) {
-    console.log('suite', suiteName)
-  }
-  console.log(testName)
-  console.log('=== error ===')
-  console.log(testError)
-  console.log('=== commands ===')
-  console.log(testCommands.join('\n'))
+  // console.log('=== test failed ===')
+  // console.log(specName)
+  // console.log('=== title ===')
+  // console.log(title)
+  // if (suiteName) {
+  //   console.log('suite', suiteName)
+  // }
+  // console.log(testName)
+  // console.log('=== error ===')
+  // console.log(testError)
+  // console.log('=== commands ===')
+  // console.log(testCommands.join('\n'))
+
+  let base1 = "cypress\\e2e\\";
+  let base2 = "cypress/e2e/";
 
   const info = {
-    specName,
+    specName: specName.includes(base1) || specName.includes(base2) ? (specName.includes(base1) ? specName.replace(base1, "") : specName.replace(base2, "")) : specName,
     title,
     suiteName,
     testName,
@@ -157,22 +152,20 @@ function onFinish () {
   }
 
   this.currentTest.commandsLogs = testCommands;
-  
-  const filepath = writeTestInfo(info)
-  console.log('saving the log file %s', filepath)
-  info.filepath = filepath
 
+  /*const filepath =*/ writeTestInfo(info)
+  // console.log('saving the log file %s', filepath)
+  // info.filepath = filepath
 }
 
 const _afterEach = afterEach
-//const _beforeEach = beforeEach
 
 afterEach = (name, fn) => {
   if (typeof name === 'function') {
     fn = name
     name = fn.name
   }
-  
+
   _afterEach(name, function () {
     onFinish.call(this)
     fn.call(this)
@@ -180,5 +173,4 @@ afterEach = (name, fn) => {
 }
 
 startLogging()
-//beforeEach(initLog)
 _afterEach(onFinish)
