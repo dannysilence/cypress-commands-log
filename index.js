6576d58509e69b5406fcf44cc4d51093aa8bdfd8
ds <dannysilence@homelabs.company>
2022-11-18 10:36:29 +0200

    ..

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 const fs = require('fs')
 const path = require('path')
-const debug = require('debug')('cypress-failed-log')
+const debug = require('debug')('cypress-commands-log')
 
 // check built-in module against missing methods
 if (typeof path.basename !== 'function') {
