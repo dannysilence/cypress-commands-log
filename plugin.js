e61ec21b20ee64386a5608e515be227861699005
dannysilence <48093199+dannysilence@users.noreply.github.com>
2022-11-18 18:14:13 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,12 +1,14 @@
-module.exports.configure = function (on) {
-    on('task', {
-      console: (method, data) => {
-        if(method === 'table') {
-            console.table(data);
-        } else 
-        if(method === 'log') {
-            console.log(data);
+module.exports.configure = function (on, config) {
+  on('task', {
+    console: (method, data) => {
+      if (method === 'table') {
+        console.table(data);
+      } else
+        if (method === 'log') {
+          console.log(data);
         }
-      }
-    })
-  }
+    }
+  })
+
+  return config;
+}   
